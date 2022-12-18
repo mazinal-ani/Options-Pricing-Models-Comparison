60af3387b295a85374b21257c59f9af427779576
Mazin Al-Ani <mazinal-ani@Mazins-MacBook-Pro.local>
2022-12-18 14:03:44 -0500

    Added basic input definition

---
+++ equations.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+///////////////////////////////////////////////////////////////////
+// Input definitions
+
+int underlying_price()
+{
+    int price;
+    cin >> price;
+    return(price);
+}
+
+int strike_price()
+{
+    int strike;
+    cin >> strike;
+    return(strike);
+}
+
+int maturity()
+{
+    int maturity;
+    cin >> maturity;
+    return(maturity);
+}
+
+int interest_rate()
+{
+    int ir;
+    cin >> ir;
+    return ir;
+}
+
+int implied_volatility()
+{
+    int iv;
+    cin >> iv;
+    return(iv);
+}
+///////////////////////////////////////////////////////////////////
+
+
+int main()
+{
+    return(0);
+
+
+}
